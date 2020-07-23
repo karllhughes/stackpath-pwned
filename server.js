9908ad7cc46aef4cb7d82f62d1618e38f1d52b54
Karl Hughes <khughes.me@gmail.com>
2020-07-23 09:35:57 -0500

    Adding caching

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const sha1 = require('sha1');
 const fetch = require('node-fetch');
+const NodeCache = require( "node-cache" );
 const port = process.env.PORT || 80;
 
-app = express();
+const app = express();
 app.use(express.json());
 app.listen(port);
+
+const hashedPasswordCache = new NodeCache({ stdTTL: 3600 });
+
+const generateResponse = (res, text, suffix) => text.includes(suffix) ?
+  res.status(400).json({message: 'Please select a more secure password. This one has already been Pwned.'}) :
+  res.status(200).json({message: 'Your password is secure and you may use it to register.'});
 
 app.post('/auth', (req, res) => {
   // Get the password from the request
@@ -17,23 +24,26 @@ app.post('/auth', (req, res) => {
     const prefix = hashedPassword.substring(0, 5);
     const suffix = hashedPassword.substring(5);
 
-    // Send the first 5 chars to pwned password API
-    fetch('https://api.pwnedpasswords.com/range/' + prefix)
-      .then(res => res.text())
-      .then(body => {
-        // Check if the response includes the suffix of the hash
-        if (body.includes(suffix)) {
-          // If so, return an error
-          res.status(400).json({message: 'Please select a more secure password. This one has already been Pwned.'});
-        } else {
-          // If not, the user is good to go.
-          res.status(200).json({message: 'Your password is secure and you may use it to register.'});
-        }
-      }).catch(err => {
+    // Check against cache
+    const cachedResults = hashedPasswordCache.get(prefix);
+    if (cachedResults) {
+      // Check if the cached result includes the suffix of the hash
+      return generateResponse(res, cachedResults, suffix);
+    } else {
+      // Send the first 5 chars to pwned password API
+      fetch('https://api.pwnedpasswords.com/range/' + prefix)
+        .then(tmp => tmp.text())
+        .then(body => {
+          // Cache the response for future use
+          hashedPasswordCache.set(prefix, body);
+          // Check if the response includes the suffix of the hash
+          return generateResponse(res, body, suffix);
+        }).catch(err => {
         // Handle any API errors
         console.error(err);
         res.status(400).json({message: 'Something went wrong.'});
       });
+    }
   } else {
     res.status(400).json({message: '"password" field not found.'});
   }
